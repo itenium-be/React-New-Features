cdacc10eb073e23d0d5562da15ad013eb94c9639
Wouter Van Schandevijl <woutervs@hotmail.com>
2021-03-18 20:40:49 +0100

    Cart imple

--- react-socks/src/components/Cart.tsx
+++ react-socks/src/components/Cart.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Cart.scss';
-// import { CartModel } from '../models';
-// import CartRepository from '../CartRepository';
+import { CartModel } from '../models';
+import CartRepository from '../CartRepository';
+import CartItem from './CartItem';
 
 const Cart: React.FC = () => (
   <div className="cart-container">
-    CART CONTENTS
+    {CartRepository.getItems().map((item: CartModel) => (
+      <CartItem key={item.sock.id} item={item} />
+    ))}
   </div>
 );
-
-export default Cart;
